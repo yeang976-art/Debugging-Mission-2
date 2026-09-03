69c52c7f518b1adf39f026ecc3cdb23067f14fa4
yeang976-art <yeang976@gmail.com>
2026-09-03 19:48:52 +0900

    chore: JWT 토큰 기한을 7일로 연장, 설정 파일에서 jpa 파트 포맷 수정

--- src/main/java/org/example/expert/config/JwtUtil.java
+++ src/main/java/org/example/expert/config/JwtUtil.java
@@ -21,7 +21,7 @@ import java.util.Date;
 public class JwtUtil {
 
     private static final String BEARER_PREFIX = "Bearer ";
-    private static final long TOKEN_TIME = 4000 * 60 * 1000L; // 60분
+    private static final long TOKEN_TIME = 7 * 24 * 60 * 60 * 1000L; // 7일
 
     @Value("${jwt.secret.key}")
     private String secretKey;
