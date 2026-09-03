2d2dce140b4333cfbed15a8f7d2eaf305d3f0771
yeang976-art <yeang976@gmail.com>
2026-09-03 19:04:10 +0900

    fix: changeUserRole() 메소드가 해당 AOP 실행 후에 동작하도록 수정

--- src/main/java/org/example/expert/domain/user/service/UserAdminService.java
+++ src/main/java/org/example/expert/domain/user/service/UserAdminService.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.entity.User;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserAdminService {
 
@@ -19,5 +21,6 @@ public class UserAdminService {
     public void changeUserRole(long userId, UserRoleChangeRequest userRoleChangeRequest) {
         User user = userRepository.findById(userId).orElseThrow(() -> new InvalidRequestException("User not found"));
         user.updateRole(UserRole.of(userRoleChangeRequest.getRole()));
+        log.info("changeUserRole 메서드 실행 완료");
     }
 }
